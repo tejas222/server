5fdb0b2991d98f12f93f750beb378d0259cb7eb8
Tejas Joshi <tejasj15@gmail.com>
2025-06-09 21:01:30 +0530

    added mongo connection

--- server.js
+++ server.js
@@ -19,3 +19,15 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("✅ Connected to MongoDB Atlas"))
+  .catch((err) => console.error("❌ MongoDB connection error:", err));
