108ccb95ad2b36a051fe286cf64927266d908807
Tejas Joshi <tejasj15@gmail.com>
2025-06-09 20:44:19 +0530

    Added root route

--- server.js
+++ server.js
@@ -12,5 +12,10 @@ app.use(express.json());
 
 app.use('/api/auth', require('./routes/authRoutes'));
 
+app.get('/', (req, res) => {
+  res.send('Attendance backend running ✅');
+});
+
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
