59431c42a56fd0a6804eb2b144fd4bf1fd1e3a7e
Tejas Joshi <tejasj15@gmail.com>
2025-06-13 22:07:58 +0530

    userRoute updated

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { getMe, getAllUsers } = require('../controllers/userController');
-const { protect, isAdmin } = require('../middleware/authMiddleware');
 
-router.get('/me', protect, getMe);
-router.get('/', protect, isAdmin, getAllUsers); // Admin only
+// Sample route
+router.get('/', (req, res) => {
+  res.json({ message: 'User route working ✅' });
+});
 
 module.exports = router;
